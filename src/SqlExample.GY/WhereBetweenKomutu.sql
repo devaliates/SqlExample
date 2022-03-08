bec54ccc90754992ab1fd6626b4b024b0ec3af64
devaliates <devaliates@gmail.com>
2022-03-08 16:48:46 +0300

    WhereBetweenKomutu eklendi.

---
+++ src/SqlExample.GY/WhereBetweenKomutu.sql
@@ -0,0 +1,9 @@
+﻿-- doğum yılı 1950 ile 1965 yılları arasında olan personelleri getir.
+select *
+from personeller
+where Year(DogumTarihi) > 1950 and year(dogumtarihi) < 1965
+
+	-- between komutu ile 
+	select *
+	from personeller
+	where year(DogumTarihi) between 1950 and 1965
