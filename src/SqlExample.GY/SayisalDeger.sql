2bbd763890fbfd6eaef635740912ec7a6686b8d3
Ali Ateş <devaliates@gmail.com>
2022-03-08 22:11:09 +0300

    SayisalDeger

---
+++ src/SqlExample.GY/SayisalDeger.sql
@@ -0,0 +1,24 @@
+﻿-- sayısal değer işlemleri
+
+select 3 + 2
+select 3 * 3
+select 4 / 2
+select 9 - 7
+
+-- pi
+select pi()
+
+-- sin
+select sin(pi())
+
+-- power üs
+select power(2, 3)
+
+-- abs mutlak değer
+select abs(-12)
+
+-- rand 0 - 1 arasında rastgele sayı üretir.
+select rand()
+
+-- floor yuvarlama
+select floor(Rand()*100)
