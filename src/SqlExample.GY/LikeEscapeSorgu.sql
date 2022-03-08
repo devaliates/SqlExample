e899b6e1bc62e2aebc81bebeec2a630af1aaada1
Ali Ateş <devaliates@gmail.com>
2022-03-08 21:17:20 +0300

    LikeEscapeSorgu eklendi.

---
+++ src/SqlExample.GY/LikeEscapeSorgu.sql
@@ -0,0 +1,10 @@
+﻿-- like sorgularında kullandığımız %, _, [] gibi özel ifadeler verilerimizin
+-- içerisinde geçiyorsa hata alırız bunun önüne geçmek için bu ifadelerin özel ifade
+-- olmadığını escape karakteri ile belirleriz.
+
+-- [] operatörü ile
+-- escape komutu ile
+
+select * from personeller where adi like '[_]%'
+
+select * from personeller where adi like 'ü_%' escape 'ü'
