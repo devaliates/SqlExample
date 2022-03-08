a985e03ecc06c0b32aa2f5141081e70064721dc3
Ali Ateş <devaliates@gmail.com>
2022-03-08 21:41:42 +0300

    AggregateFonksiyonlari eklendi.

---
+++ src/SqlExample.GY/AggregateFonksiyonlari.sql
@@ -0,0 +1,17 @@
+﻿-- bir tablo daki tüm veriler ve tüm satırlarla işlem yapar
+
+-- avg ortalama
+select avg(PersonelId) from personeller
+
+-- max en büyük değeri bul
+select max(PersonelId) from personeller
+ 
+-- min en küçük değer
+select min(personelId) from personeller
+
+-- count toplam sayısını bul (satır sayısı)
+select count(*) from personeller
+select count(Adi) from personeller
+
+-- sum toplamı
+select sum(nakliyeucreti) from satislar
