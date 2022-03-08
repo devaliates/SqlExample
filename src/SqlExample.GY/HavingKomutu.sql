b15b7243f8c32512a804897b1bd445c308cee956
Ali Ateş <devaliates@gmail.com>
2022-03-08 23:00:42 +0300

    HavingKomutu

---
+++ src/SqlExample.GY/HavingKomutu.sql
@@ -0,0 +1,18 @@
+﻿-- aggregate fonksiyonu üzerinde şart sağlamak için kullanılır.
+
+select * from urunler
+
+select kategoriId, count(*)
+from urunler
+where kategoriId > 5
+group by kategoriId
+having count(*) > 6
+
+select personelId, count(*)
+from satislar
+where personelId < 4
+group by personelId
+
+select personelId, sum(nakliyeUcreti)
+from satislar
+group by personelId
