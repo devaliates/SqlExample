1d2e7832183a92ddd6b96b71702d37e1c29b9981
devaliates <devaliates@gmail.com>
2022-03-08 16:51:41 +0300

    WhereInKomutu eklendi.

---
+++ src/SqlExample.GY/WhereInKomutu.sql
@@ -0,0 +1,9 @@
+﻿-- yaşadığı şehrin london, tacoma ve krikland olan personellerin adını listeleyelim.
+select Adi
+from personeller
+where sehir = 'london' or sehir = 'tacoma' or sehir = 'kirkland'
+
+	-- In komutu ile
+	select Adi
+	from personeller
+	where sehir in('london', 'tacoma', 'kirkland')
