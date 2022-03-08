ef06056cb91207569e490cb30bd70721dc9cfe1e
Ali Ateş <devaliates@gmail.com>
2022-03-08 22:29:02 +0300

    TarihFonksiyonlari

---
+++ src/SqlExample.GY/TarihFonksiyonlari.sql
@@ -0,0 +1,17 @@
+﻿-- getdate bu günün tarihi
+select getDate()
+
+-- dateAdd verilen tarihe verildiği kadar gün, ay, yıl ekler.
+select dateAdd(day, 999, getDate())
+select dateAdd(month, 999, getDate())
+select dateAdd(year, 999, getDate())
+
+-- dateDiff iki tarih arasında günü, ayı ve ya yılı hesaplar.
+select dateDiff(day, '05.15.1992', getDate())
+select dateDiff(month, '05.15.1992', getDate())
+select dateDiff(year, '05.15.1992', getDate())
+
+-- datePart verilen tarihin haftanın, ayın yahut yılın kaçıncı günü olduğunu hesaplar.
+select datePart(dw, getDate())
+select datePart(month, getDate())
+select datePart(day, getDate())
