a1733a84f2dc96a3f9d695350f34125fef38354a
devaliates <devaliates@gmail.com>
2022-03-08 08:47:50 +0300

    Degiskenler eklendi.

---
+++ src/SqlExample.Kodlab/Degiskenler.sql
@@ -0,0 +1,37 @@
+﻿-- Değişkenlere değer atamak
+
+DECLARE @sayi1 INT;
+DECLARE @sayi2 INT;
+DECLARE @toplam INT = @sayi1 + @sayi2;
+
+SELECT @toplam;
+
+DECLARE @sayi1 INT = 12;
+DECLARE @sayi2 INT = 12;
+DECLARE @toplam INT = @sayi1 + @sayi2;
+
+SELECT @toplam;
+
+-- SET ile değişkene değer atamak
+
+DECLARE @Fiyat MONEY;
+DECLARE @KdvOran MONEY;
+DECLARE @KdvMiktar MONEY;
+DECLARE @Toplam MONEY;
+
+SET @Fiyat = 10;
+SET @KdvOran = 0.18;
+SET @KdvMiktar = @Fiyat * @KdvOran;
+SET @Toplam = @Fiyat + @KdvMiktar;
+
+SELECT @Toplam;
+
+-- SELECT ile değişkene değer atamak
+
+DECLARE @EnYuksekFiyat MONEY;
+SET @EnYuksekFiyat = (SELECT MAX(ListPrice) FROM Production.Product);
+SELECT @EnYuksekFiyat AS EnYuksekFiyat;
+
+DECLARE @EnYuksekFiyat MONEY;
+SELECT @EnYuksekFiyat = (SELECT MAX(ListPrice) FROM Production.Product);
+SELECT @EnYuksekFiyat AS EnYuksekFiyat;
