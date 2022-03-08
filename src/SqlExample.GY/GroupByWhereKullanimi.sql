782f99f5e6f44173ecf185dba5a933caff71f18c
Ali Ateş <devaliates@gmail.com>
2022-03-08 22:55:18 +0300

    GroupByWhereKullanimi

---
+++ src/SqlExample.GY/GroupByWhereKullanimi.sql
@@ -0,0 +1,15 @@
+﻿select * from urunler
+
+select kategoriId, count(*)
+from urunler
+where kategoriId > 5
+group by kategoriId
+
+select personelId, count(*)
+from satislar
+where personelId < 4
+group by personelId
+
+select personelId, sum(nakliyeUcreti)
+from satislar
+group by personelId
