0c798d1445ba7f42ea3cdefb33dd0a1abc4e8e28
Ali Ateş <devaliates@gmail.com>
2022-03-08 23:11:20 +0300

    TablolariYanYanaBirlestirme

---
+++ src/SqlExample.GY/TablolariYanYanaBirlestirme.sql
@@ -0,0 +1,18 @@
+﻿select * from personeller
+select * from satislar
+
+select * from personeller, satislar
+
+-- benzer isimdeki kolonlar için
+select personelId from personeller, satislar
+
+-- hangi tablodan personelId çekileceğini belirttik.
+select personeller.personelId from personeller, satislar
+
+-- alias eklenerek daha kolay gösterim.
+select p.personelId, s.musteriId from personeller p, satislar s
+
+-- where
+select p.personelId, s.musteriId
+from personeller p, satislar s
+where p.personelId = s.personelId
