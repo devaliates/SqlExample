d98a2f1e4f1794a3a59496290992b1fc0da957c4
Ali Ateş <devaliates@gmail.com>
2022-03-08 22:48:51 +0300

    GroupByKomutu

---
+++ src/SqlExample.GY/GroupByKomutu.sql
@@ -0,0 +1,9 @@
+﻿-- selecte normal kolon ve aggregate varsa group by kullanılmalıdır!
+
+select * from urunler
+
+select kategoriId, count(*) from urunler group by kategoriId
+
+select personelId, count(*) from satislar group by personelId
+
+select personelId, sum(nakliyeUcreti) from satislar group by personelId
