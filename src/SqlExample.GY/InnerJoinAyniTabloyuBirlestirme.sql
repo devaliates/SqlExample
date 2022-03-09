43b3069ace6ce955ae3b6fa079a80d0c0976d2d7
Ali Ateş <devaliates@gmail.com>
2022-03-09 20:32:59 +0300

    InnerJoinAyniTabloyuBirlestirme

---
+++ src/SqlExample.GY/InnerJoinAyniTabloyuBirlestirme.sql
@@ -0,0 +1,9 @@
+﻿
+-- aynı tabloyu ilişkisel olarak birleştirme 
+
+-- personellerimin bağlı olarak çalıştığı kişileri listele?
+-- (personeller, personeller)
+
+select p1.adi, p2.adi
+from personeller p1
+inner join personeller p2 on p1.bagliCalistigiKisi = p2.personelId
