1bf777a0517f0bb08a202af26f18eed2f1b1b83b
Ali Ateş <devaliates@gmail.com>
2022-03-10 20:22:59 +0300

    InnerJoinGroupBy ekleniyor.

---
+++ src/SqlExample.GY/InnerJoinGroupBy.sql
@@ -0,0 +1,12 @@
+﻿-- hangi personelim (adı ve soyadı ile birlikte),
+-- toplam kaç adetlik satış yapmıştır.
+-- satış adedi 100 den fazla olanlar ve personelin adının
+-- baş harfi M olan kayıtlar gelsin. (personeller, satislar)
+select p.adi + ' ' + p.soyadi, count(s.satisId)
+from personeller p
+inner join satislar s on p.personelId = s.personelId
+where p.adi like'm%' group by p.adi + ' ' + p.soyadi
+having count(s.satisId) > 100
+
+
+-- https://www.youtube.com/watch?v=e3E2sg1Qkdk&list=PLQVXoXFVVtp2RjHt5teaBOLUcKbq2Ilbo&index=23
